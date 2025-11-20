3522ae1db9c2434802788b8d7528f4eecfc3dd86
Irfansyah001 <syahirfan545@gmail.com>
2025-11-20 16:23:35 +0700

    refactor: Replace Greeting with SiswaApp in MainActivity
    Removes the `Greeting` composable and its preview.
    Integrates the `SiswaApp` composable into the `MainActivity` to serve as the main application content.
    Also adds a new `studiobot.xml` configuration file to opt-in to context sharing.

--- app/src/main/java/com/example/navigasi/MainActivity.kt
+++ app/src/main/java/com/example/navigasi/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.navigasi.ui.theme.NavigasiTheme
+import com.example.navigasi.view.uicontroller.SiswaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,28 +21,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             NavigasiTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    SiswaApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NavigasiTheme {
-        Greeting("Android")
     }
 }
