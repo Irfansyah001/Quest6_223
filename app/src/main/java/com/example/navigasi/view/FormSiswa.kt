15d5462e16101819a4f6cc8d12233a98ac56dddd
Irfansyah001 <syahirfan545@gmail.com>
2025-11-20 15:57:53 +0700

    feat: Add FormSiswa composable function
    Adds a new Composable function `FormSiswa` which takes a list of strings `pilihanJK` and an `onSubmitButtonClicked` lambda function. It is annotated with `@OptIn(ExperimentalMaterial3Api::class)`.

--- app/src/main/java/com/example/navigasi/view/FormSiswa.kt
+++ app/src/main/java/com/example/navigasi/view/FormSiswa.kt
@@ -32,3 +32,11 @@ import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.example.navigasi.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormSiswa(
+    pilihanJK: List<String>,
+    onSubmitButtonClicked: (MutableList<String>) -> Unit,
+    modifier: Modifier = Modifier
+){
