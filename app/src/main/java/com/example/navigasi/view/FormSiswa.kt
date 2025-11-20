82bcc9e02bad91a927a3733f5a2b5d0f32f0f656
Irfansyah001 <syahirfan545@gmail.com>
2025-11-20 15:56:44 +0700

    feat: Import additional composables into FormSiswa
    Imports Scaffold, Text, TopAppBar, TopAppBarDefaults, Composable, remember, mutableStateOf, and getValue into `FormSiswa.kt`.

--- app/src/main/java/com/example/navigasi/view/FormSiswa.kt
+++ app/src/main/java/com/example/navigasi/view/FormSiswa.kt
@@ -15,3 +15,11 @@ import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
