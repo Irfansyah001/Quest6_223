c54464726df078f24dab7f3776e89752678660f1
Irfansyah001 <syahirfan545@gmail.com>
2025-11-20 15:55:34 +0700

    feat: Create FormSiswa.kt and add initial imports
    Creates a new file `FormSiswa.kt` within the `com.example.navigasi.view` package.
    
    Adds initial Jetpack Compose layout imports, including `Arrangement`, `Column`, `Row`, `Spacer`, `fillMaxWidth`, `height`, and `padding`.

---
+++ app/src/main/java/com/example/navigasi/view/FormSiswa.kt
@@ -0,0 +1,9 @@
+package com.example.navigasi.view
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
